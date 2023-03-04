02aea66414318f1eeec38e60a743c241375f65e7
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2023-03-04 13:19:21 +0700

    Добавлены типы данных

---
+++ project/src/types/state.ts
@@ -0,0 +1,27 @@
+import { store } from '../store/store';
+import { Camera } from './camera';
+import { Promo } from './promo';
+import { Review } from './review';
+
+export type State = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type CamerasLoad = {
+  cameras: null | Camera[];
+  total: number;
+  areCamerasLoading: boolean;
+}
+export type PromoLoad = {
+  promo: null | Promo;
+  isPromoLoading: boolean;
+}
+export type ItemLoad = {
+  selectedCamera: null | Camera;
+  isSelectedCameraLoading: boolean;
+  hasSelectedCameraLoadingError: boolean;
+  similarItems: null | Camera[];
+  areSimilarItemsLoading: boolean;
+  reviews: null | Review[];
+  areReviewsLoading: boolean;
+  isReviewFormBlocked: boolean;
+  addedReview: null | Review;
+}
