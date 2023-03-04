8ee799424496b9288664bca102f5cffc2f09b36d
Fedor Medvedev <fedor_medvedev42@rambler.ru>
2023-03-04 14:00:36 +0700

    Добавлены асинхронные действия и селекторы для загрузки рекламного предложения с сервера

---
+++ project/src/store/promo-load/promo-load.ts
@@ -0,0 +1,28 @@
+import { createSlice/*, PayloadAction*/ } from '@reduxjs/toolkit';
+import { NameSpace } from '../../const';
+import { PromoLoad } from '../../types/state';
+import { fetchPromoAction } from './api-actions';
+
+const initialState: PromoLoad = {
+  promo: null,
+  isPromoLoading: false
+};
+
+export const promoLoad = createSlice({
+  name: NameSpace.Promo,
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPromoAction.pending, (state) => {
+        state.isPromoLoading = true;
+      })
+      .addCase(fetchPromoAction.fulfilled, (state, action) => {
+        state.isPromoLoading = false;
+        state.promo = action.payload;
+      })
+      .addCase(fetchPromoAction.rejected, (state) => {
+        state.isPromoLoading = false;
+      });
+  }
+});
